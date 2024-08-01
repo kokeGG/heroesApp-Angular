f0a9efea4e3542314147c581bb85463be3d48467
kokeGG <kokehr99@gmail.com>
2024-07-31 18:44:20 -0600

    Material Dialog

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -6,6 +6,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Hero, Publisher } from '../../interfaces/hero.interfaces';
 import { HeroesService } from '../../services/heroes.service';
 import { switchMap } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
@@ -34,7 +36,8 @@ export class NewPageComponent implements OnInit {
     private heroesService: HeroesService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -76,6 +79,25 @@ export class NewPageComponent implements OnInit {
         this.router.navigate([ '/heroes/edit', hero.id ]);
         this.showSnackbar(`${hero.superhero} created!`);
       })
+  }
+
+  onDeleteHero() {
+    if ( !this.currentHero.id ) {
+      throw Error('Hero id is required');
+    }
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (!result) return;
+
+      this.heroesService.deleteHeroById( this.currentHero.id );
+      this.router.navigate(['/heroes'])
+
+    });
+
   }
 
   showSnackbar(message: string):void {
